23dc37ea7d838972eec127ba27c7bb8adf8ba369
zengd <309606298@qq.com>
2022-08-29 15:00:52 +0800

    rabbitmq 消息确认配置

--- rabbitmq/src/main/java/com/zengdw/rabbitmq/controller/SendMessageController.java
+++ rabbitmq/src/main/java/com/zengdw/rabbitmq/controller/SendMessageController.java
@@ -1,5 +1,7 @@
 package com.zengdw.rabbitmq.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +26,7 @@ public class SendMessageController {
     private RabbitTemplate rabbitTemplate;
 
     @GetMapping("/sendDirectMessage")
-    public String sendDirectMessage() {
+    public String sendDirectMessage() throws JsonProcessingException {
         String messageId = String.valueOf(UUID.randomUUID());
         String messageData = "test message, hello!";
         String createTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
@@ -32,8 +34,21 @@ public class SendMessageController {
         map.put("messageId",messageId);
         map.put("messageData",messageData);
         map.put("createTime",createTime);
-        //将消息携带绑定键值：DirectRouting 发送到交换机DirectExchange
-        rabbitTemplate.convertAndSend("DirectExchange", "DirectRouting", map);
+        ObjectMapper mapper = new ObjectMapper();
+        String s = mapper.writeValueAsString(map);
+
+        // 消息发送到交换器确认
+        rabbitTemplate.setConfirmCallback((var1, var2, var3) -> {
+            // var2 true 表示消息成功发送到交换器 false 发送失败
+            // var3 失败信息
+            System.out.println(var1);
+            System.out.println(var2);
+            System.out.println(var3);
+        });
+        // 消息从交换器到队列确认 只有失败时才会回调次方法
+        rabbitTemplate.setReturnsCallback(System.out::println);
+        // 将消息携带绑定键值：DirectRouting 发送到交换机DirectExchange
+        rabbitTemplate.convertAndSend("DirectExchange", "DirectRouting", s);
         return "ok";
     }
 }
--- rabbitmq/src/main/java/com/zengdw/rabbitmq/receiver/DirectReceiver.java
+++ rabbitmq/src/main/java/com/zengdw/rabbitmq/receiver/DirectReceiver.java
@@ -1,10 +1,14 @@
 package com.zengdw.rabbitmq.receiver;
 
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 /**
  * 消费者
@@ -15,9 +19,29 @@ import java.util.Map;
  */
 @Component
 public class DirectReceiver {
+    /**
+     * ackMode  MANUAL：手动确认 AUTO：自动确认
+     */
     @RabbitHandler
-    @RabbitListener(queues = "DirectQueue")
-    public void process(Map<String, Object> testMessage) {
-        System.out.println("DirectReceiver消费者收到消息  : " + testMessage.toString());
+    @RabbitListener(queues = "DirectQueue", ackMode = "MANUAL")
+    public void process(Message message, Channel channel) {
+        MessageProperties properties = message.getMessageProperties();
+        try {
+            String s = new String(message.getBody(), StandardCharsets.UTF_8);
+            System.out.println("DirectReceiver消费者收到消息  : " + s);
+            channel.basicAck(properties.getDeliveryTag(), false);
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                // 第二个参数 true表示应用于多条序消息 第三个参数 true 重新放入队列
+                // channel.basicNack(properties.getDeliveryTag(), false, true);
+                // 第二个参数 true表示重新放入队列
+                // channel.basicReject(properties.getDeliveryTag(), true);
+                // 是否恢复消息到队列，参数默认为true，true则重新入队列，并且尽可能的将之前recover的消息投递给其他消费者消费，而不是自己再次消费。false则消息会重新被投递给自己
+                channel.basicRecover(false);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
     }
 }
